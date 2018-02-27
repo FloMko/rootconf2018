2ae405c3d02290134c82467ae690e8e57834cd05
maksim <maskim.ermolenko@tra.ai>
2018-02-27 23:41:19 +0300

    create count dict

--- ex5.py
+++ ex5.py
@@ -9,23 +9,30 @@ def input_read():
 
 def prepare_dict(input_dict):
     prod_dict = {rows['src_user']: {'output_byte':0, 'count':0} for rows in input_dict}
-    return(prod_dict, input_dict)
+    return(prod_dict)
 
 
+def count_sorted(prod_dict):
+    gen_dict = input_read()
+    for rows in gen_dict:
+        src_user = rows['src_user']
+        prod_dict.get(src_user)['count'] +=1
+    return prod_dict
 
-def find_max_queue(prod_dict, input_dict):
-    for rows in input_dict:
-        return None
+def find_max_queue(prod_dict):
+    return count_sorted[:4] 
 
 
 def generate_answer():
-    prod_dict, input_dict = prepare_dict(input_read())
+    prod_dict = prepare_dict(input_read())
+    count_d
     with open('ex5_out.txt', mode='w') as outfile:
         outfile.write(
             '# Поиск 5ти пользователей, сгенерировавших наибольшее\
              количество запросов')
-        outfile.write(find_max_queue(prod_dict, input_dict))
+        outfile.write(find_max_queue(prod_dict))
 
 
 if __name__ == '__main__':
     generate_answer()
+
